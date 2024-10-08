dd7c5d29f067783702194ecc8aff9ef9b370d4a3
멍멍민 <pr620718@cu.ac.kr>
2024-10-08 22:38:09 +0900

    💚 Chore: 개발 환경 초기 설정 완료

---
+++ Week01/HelloWorld.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        #region I/O
+        // 콘솔의 입출력 인코딩을 UTF-8로 설정
+        Console.InputEncoding = Encoding.UTF8;
+        Console.OutputEncoding = Encoding.UTF8;
+
+        using var read = new System.IO.StreamReader(Console.OpenStandardInput());
+        using var print = new System.IO.StreamWriter(Console.OpenStandardOutput()) { AutoFlush = true };
+        #endregion
+
+        print.Write("Hello, world!");
+    }
+}
